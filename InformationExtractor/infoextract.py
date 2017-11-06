0dbe4fa0c35243148510c2fb0d3a7552ff62bbee
Jesus <jesus.zarate.eng@gmail.com>
2017-11-05 21:53:03 -0700

    Returning individual templates based on the name

--- InformationExtractor/infoextract.py
+++ InformationExtractor/infoextract.py
@@ -1,31 +1,42 @@
 import sys
 from nltk.corpus import wordnet
 from pathlib import Path
+import re
+from InformationExtractor import IO as io
 
 FILEPATH = "devetexts/answers/texts/"
 
 
 class infoExtract:
     def __init__(self, filename):
-        self.tokens = self.readFile(filename)
+        # self.tokens = self.readFile(filename)
+        self.template = io.FileOutputTemplate
+        self.file = io.readFile(filename)
 
-    def readFile(self, filename):
-        tokens = []
-        with open(filename, 'r') as f:
-            for line in f:
-                tokens.extend(line.split())
-        return tokens
+    # def readFile(self, filename):
+    #     tokens = []
+    #     with open(filename, 'r') as f:
+    #         for line in f:
+    #             tokens.extend(line.split())
+    #     return tokens
+
+    def getID(self):
+        m = re.search('[a-zA-Z0-9_]+-[a-zA-Z0-9_]+-[a-zA-Z0-9_]+', self.file)
+        #print(m.group(0))
+        return m.group(0)
+
+    def compute(self):
+        self.template[io.ID['label']] = self.getID()
+
+    def getResults(self):
+        return self.template
 
 
-def getFilePath(filename):
-    return FILEPATH + filename
+# def getFilePath(filename):
+#     return FILEPATH + filename
+#
+#
 
-
-def isFile(filename):
-    file = Path(getFilePath(filename))
-    if file.is_file():
-        return True
-    return False
 
 
 def main(argv):
@@ -34,32 +45,35 @@ def main(argv):
         print('Note: Make sure the input files are stored in the eval-program-files folder')
         sys.exit(2)
 
-    if not (isFile(argv[0])):
+    if not (io.isFile(argv[0])):
         print('In argument 1 file does not exist: {0}'.format(argv[0]))
         print('Note: Make sure the input files are stored in the developset/texts/ folder')
         sys.exit(2)
 
-    ie = infoExtract(getFilePath(argv[0]))
+    ie = infoExtract(argv[0])
+    ie.compute()
+
+    io.writeTemplate(ie.getResults(), 'outputFile')
 
 if __name__ == '__main__':
 
-    syns = wordnet.synsets("playing")
-
-    print(syns[0].name())
-    print(syns[0].lemmas()[0].name())
-
-    print(syns[0].definition())
-
-    synonyms = []
-    antonyms = []
-
-    for syn in wordnet.synsets("good"):
-        for l in syn.lemmas():
-            synonyms.append(l.name())
-            if l.antonyms():
-                antonyms.append(l.antonyms()[0].name())
-
-    print(synonyms)
-    print(antonyms)
+    # syns = wordnet.synsets("playing")
+    #
+    # print(syns[0].name())
+    # print(syns[0].lemmas()[0].name())
+    #
+    # print(syns[0].definition())
+    #
+    # synonyms = []
+    # antonyms = []
+    #
+    # for syn in wordnet.synsets("good"):
+    #     for l in syn.lemmas():
+    #         synonyms.append(l.name())
+    #         if l.antonyms():
+    #             antonyms.append(l.antonyms()[0].name())
+    #
+    # print(synonyms)
+    # print(antonyms)
 
     main(sys.argv[1:])
