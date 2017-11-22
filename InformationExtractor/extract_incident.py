f29594599d639fdf382e4dbc197a665b75fc4e50
Jesus <jesus.zarate.eng@gmail.com>
2017-11-21 20:37:01 -0700

    Cleaned up and getting 31 f score

--- InformationExtractor/extract_incident.py
+++ InformationExtractor/extract_incident.py
@@ -2,29 +2,45 @@ from InformationExtractor import IO as io
 import nltk
 import re
 
+
 def createRegex(regDict):
     res = ''
     for k, v in regDict.items():
         for item in v:
             res += '(\\b{0}\\b)|'.format(item)
-    return res#[:-1]
+    return res[:-1]
+
+
+regexValues = io.read_regex()
+regex = createRegex(regexValues)
+
 
 def get_incident(story):
-    regexValues = io.read_regex()
-    regex = createRegex(regexValues)
-    print(regex)
-
     content = story.lower()
     words = nltk.word_tokenize(content)
 
+    l = {}
+    res = 'ATTACK'
     for word in words:
         m = re.match(r'(' + regex + ')', word)
         if m != None:
             val = m.group(0)
             for k, vals in regexValues.items():
-                if val == 'taken':
-                    return 'ATTACK'
                 if val in vals:
-                    return k.upper()
 
-    return 'ATTACK'
+                    if val not in l:
+                        l[k.upper()] = 0
+                    l[k.upper()] += 1
+
+    max_key = res
+    if len(l) > 0:
+        max_key = max(l, key=lambda k: l[k])
+
+    if max_key != 'ATTACK':
+        return max_key
+
+    if len(l) > 1:
+        del l[max_key]
+        max_key = max(l, key=lambda k: l[k])
+
+    return max_key
